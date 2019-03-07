c94057b861df8e45c993bc69d59256d3c9bbd2b5
Khistunning <43241730+Khistunning@users.noreply.github.com>
2019-03-07 09:23:31 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+$("button").click(function(){
+$("#direction").show();
+$("#direction").text("double click");
+
+
+});
+$("img").hide();
+$("#direction").dblclick(function(){
+    $("#img22").show();});
+    $("#img22").dblclick(function(){
+        $("#img22").text("click now");
+    });
+$("#gd").dblclick(function(){
+    
+    
+    
+    
+});
+        
+        
